ea4aa041d08ad9d86749922761ec651b6e5236f1
Liam Cary <liam.cary@laigames.com>
2022-06-27 23:40:52 +0800

    Add custom inspector with buttons to generate analyses

---
+++ Assets/Editor/ShaderAnalyzerDataInspector.cs
@@ -0,0 +1,24 @@
+using UnityEditor;
+using UnityEngine;
+
+namespace UnityShaderAnalyzer
+{
+	[CustomEditor(typeof(ShaderAnalyzerData))]
+	public class ShaderAnalyzerDataInspector : Editor
+	{
+		public override void OnInspectorGUI()
+		{
+			DrawDefaultInspector();
+
+			if (GUILayout.Button("Generate Android Analyses - Text")) {
+				var data = (ShaderAnalyzerData) target;
+				data.AnalyzeAllText(UnityEditor.Rendering.ShaderCompilerPlatform.GLES3x, UnityEngine.Rendering.GraphicsTier.Tier1, BuildTarget.Android);
+			}
+
+			if (GUILayout.Button("Generate Android Analyses - Json")) {
+				var data = (ShaderAnalyzerData) target;
+				data.AnalyzeAllJson(UnityEditor.Rendering.ShaderCompilerPlatform.GLES3x, UnityEngine.Rendering.GraphicsTier.Tier1, BuildTarget.Android);
+			}
+		}
+	}
+}
